17ccd321dd71314a1381d2c2feda9e0b44a855f3
phynicsmusic <thawzin19992099@gmail.com>
2026-03-29 21:46:29 +0630

    Update play.py

--- MYANMARMUSIC/utils/inline/play.py
+++ MYANMARMUSIC/utils/inline/play.py
@@ -88,10 +88,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(text="🌲sᴜᴘᴘᴏʀᴛ🌲", url=SUPPORT_GROUP),
-            InlineKeyboardButton(text="🌻ᴄʜᴀɴɴᴇʟ🌻", url=SUPPORT_CHANNEL),
-        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
